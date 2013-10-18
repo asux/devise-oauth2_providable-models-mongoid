42621c2d026ced08df0df21e7314315f5a9f36fb
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2013-10-18 17:52:03 +0300

    Added tokens expire settings from `devise-oauth2_providable`

--- lib/devise/oauth2_providable/models/mongoid/engine.rb
+++ lib/devise/oauth2_providable/models/mongoid/engine.rb
@@ -3,6 +3,10 @@ module Devise
     module Models
       module Mongoid
         class Engine < ::Rails::Engine
+          config.devise_oauth2_providable = ActiveSupport::OrderedOptions.new
+          config.devise_oauth2_providable.access_token_expires_in       = 15.minutes
+          config.devise_oauth2_providable.refresh_token_expires_in      = 1.month
+          config.devise_oauth2_providable.authorization_code_expires_in = 1.minute
         end
       end
     end
--- lib/devise/oauth2_providable/models/mongoid/version.rb
+++ lib/devise/oauth2_providable/models/mongoid/version.rb
@@ -2,7 +2,7 @@ module Devise
   module Oauth2Providable
     module Models
       module Mongoid
-        VERSION = "1.0.1"
+        VERSION = "1.0.2"
       end
     end
   end
